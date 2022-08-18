816d5c3441c926c2c2c8851b1bc1bbea896d44b8
Musa Opaluwa <innovate.musa.opaluwa@gmail.com>
2022-08-18 12:12:29 +0100

    search and edited css

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import store from "./store";
-import { Provider } from "react-redux";
-
-import AddNoteForm from "./components/AddNoteForm";
-import NoteList from "./components/NoteList";
-
-import "./styles.scss";
-
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="note-taking-app">
-        <NoteList />
-        <AddNoteForm />
-      </div>
-    </Provider>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
